2bd46dd594264826e4a8ecdb5210f73ce76a3070
osadchik <48530948+osadchik@users.noreply.github.com>
2019-03-13 22:07:12 +0300

    Add files via upload

---
+++ proga.cpp
@@ -0,0 +1,20 @@
+#include "pch.h"
+#include <iostream>
+#include <fstream>
+#include <ctime>;
+
+int main()
+{
+	int n;
+	std::cout << "Input n: ";
+	std::cin >> n;
+	std::ofstream out;
+	out.open("D:\\ourfile.txt");
+	srand(time(NULL));
+	for (int i = 0; i < n; i++)
+	{
+		int number = rand() % 100 + 1;
+		out << number << std::endl;
+	}
+	out.close();
+}
