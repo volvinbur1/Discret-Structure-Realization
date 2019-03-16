7f3c879f23e9b40b9f0668e0b974c08dcc344c25
Volodymyr Burdeinyi <44869508+volvinbur1@users.noreply.github.com>
2019-03-16 06:52:00 +0200

    Update Lab1DS_DLL.cpp

--- Lab1DS_DLL.cpp
+++ Lab1DS_DLL.cpp
@@ -1,5 +1,6 @@
 //Doubly linked list
 #include <iostream>
+#include <ctime>
 
 using namespace std;
 
@@ -11,13 +12,13 @@ struct DLL
 
 void insertToFront(int _element, DLL *_DLlist)
 {
-	DLL * temp_DLL = new DLL();
+	DLL *temp_DLL = new DLL();
 
 	temp_DLL = _DLlist;
 	while (temp_DLL->prev != NULL)
 		temp_DLL = temp_DLL->prev;
 
-	DLL * insertToFront_Node = new DLL();
+	DLL *insertToFront_Node = new DLL();
 
 	insertToFront_Node->data = _element;
 	insertToFront_Node->prev = NULL;
@@ -32,9 +33,11 @@ void insertToEnd(int _data, DLL **_DLlist)
 
 	temprorary_DLlist->data = _data;
 
-	if (*_DLlist != NULL)
+	temprorary_DLlist->prev = *(_DLlist);
+
+	if ((*_DLlist)->data == NULL)
 	{
-		temprorary_DLlist->prev = *(_DLlist);
+		temprorary_DLlist->prev = NULL;
 	}
 	temprorary_DLlist->next = NULL;
 	(*_DLlist)->next = temprorary_DLlist;
@@ -43,59 +46,104 @@ void insertToEnd(int _data, DLL **_DLlist)
 
 void insertElemnt(int _element, int _after_which, DLL *_DLlist)
 {
-	DLL * temp_DLL = new DLL();
+	DLL *temp_DLL = new DLL();
 
 	temp_DLL = _DLlist;
 	
 	while (temp_DLL->data != _after_which)
 		temp_DLL = temp_DLL->prev;
 
-	DLL * insert_Node = new DLL();
+	DLL *insert_Node = new DLL();
 
 	insert_Node->data = _element;
 	insert_Node->prev = temp_DLL;
 	insert_Node->next = temp_DLL->next;
 
 	temp_DLL->next->prev = insert_Node;
+	temp_DLL->next = insert_Node;
 }
 
-void printList(DLL *_DLlist)
+void printList(DLL *_DLlist, bool _is_from_end_to_front)
 {
-	DLL * temp_DLL = new DLL();
+	DLL *temp_DLL = new DLL();
 
 	temp_DLL = _DLlist;
-	while (temp_DLL->prev != NULL)
+
+	if (_is_from_end_to_front)
+		while (temp_DLL != NULL)
+		{
+			printf("%d ", temp_DLL->data);
+			temp_DLL = temp_DLL->prev;
+		}
+	else
 	{
-		printf("%d ", temp_DLL->data);
-		temp_DLL = temp_DLL->prev;
+		while (temp_DLL->prev != NULL)
+			temp_DLL = temp_DLL->prev;
+
+		while (temp_DLL != NULL)
+		{
+			printf("%d ", temp_DLL->data);
+			temp_DLL = temp_DLL->next;
+		}
 	}
+	delete temp_DLL;
+
+	printf("\n");
+}
+
+void deleteDLL(DLL **_DLlist)
+{
+	while ((*_DLlist)->prev != NULL)
+	{
+		DLL *temp_DLL = new DLL();
+
+		temp_DLL = (*_DLlist)->prev;
+
+		delete (*_DLlist);
+
+		(*_DLlist) = temp_DLL;
+		//delete temp_DLL;
+	}
+
+	(*_DLlist)->data = NULL;
+	(*_DLlist)->next = NULL;
+	(*_DLlist)->prev = NULL;
 }
 
 int main()
 {
-	DLL *DLlist = new DLL(); // declare a head element
+	DLL *DLlist = new DLL();
 
 	DLlist->data = NULL;
 	DLlist->next = NULL;
 	DLlist->prev = NULL;
 
-	for (int i = 1; i <= 5; i++)
+	clock_t startTime = clock();
+
+	/*for (int i = 1; i <= 5; i++)
 	{
 		insertToEnd(i, &DLlist);
-	}
+	}*/
 
-	printList(DLlist);
+	printList(DLlist, false);
 
 	insertElemnt(10, 3, DLlist);
 
-	printf("\n");
-
-	printList(DLlist);
+	printList(DLlist, true);
 
 	insertToFront(17, DLlist);
+	insertToFront(8, DLlist);
 
-	printf("\n");
+	printList(DLlist, false);
 
-	printList(DLlist);
+	deleteDLL(&DLlist);
+
+	for (int i = 10; i <= 15; i++)
+		insertToEnd(i, &DLlist);
+	
+	printList(DLlist, false);
+
+	printf_s("\nThe code was running for %.2fs\n", (double)(clock() - startTime) / CLOCKS_PER_SEC);
+
 	system("Pause");
 }
